953e5d950bcca26bf7d2e12c1a4b2481cbcd9b3f
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-04-02 00:31:53 -0700

    implement collection views

---
+++ TableView-practice/TableView-practice/Mode/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  TableView-practice
+//
+//  Created by Koki Tabayashi on 2018/04/01.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import Foundation
---
+++ TableView-practice/TableView-practice/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  TableView-practice
+//
+//  Created by Koki Tabayashi on 2018/04/01.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
